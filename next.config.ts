5aa8df1a240107c3591954240885833f51d0863c
Zach Estrella <zachgabriel27@gmail.com>
2026-05-09 23:05:56 +0800

    Add pupeteer in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@sparticuz/chromium"],
+  serverExternalPackages: ["puppeteer-core","@sparticuz/chromium"],
 };
 
 export default nextConfig;
