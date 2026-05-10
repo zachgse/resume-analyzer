7c3f4c0633b217ae4f1c5cfb5ad6330636018d4b
Zach Estrella <zachgabriel27@gmail.com>
2026-05-10 23:15:06 +0800

    Update note contents in initial form

--- app/components/form/InitialForm.tsx
+++ app/components/form/InitialForm.tsx
@@ -93,7 +93,7 @@ const InitialForm = ({
     return (
         <form onSubmit={handleSubmit(submitHandler)} 
             className="lg:w-3/5 md:w-4/5 w-full md:mb-0 mb-24">
-            <p className="font-semibold text-gray-500 text-xs my-4 md:p-0 p-4">Note: Fill up the fields to generate your AI-powered resume with Google Gemini model</p>
+            <p className="font-semibold text-gray-500 text-xs my-4 md:p-0 p-4">Note: Fill up the fields to generate your AI-powered ATS format resume with Google Gemini model</p>
             <div className="grid md:grid-cols-2 grid-cols-1 md:gap-12 gap-4 md:p-0 p-4">
                 <div className="flex flex-col space-y-4">
                     <div className="w-full flex md:flex-row flex-col items-start md:gap-4 gap-0">
